f013d2f4376b19d5abd444d4afbf25a37abfac10
Omega <theomegakidd@gmail.com>
2019-10-05 12:08:21 -0700

    Added first version of the Goal.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/Goal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Goal : IComparable<Goal>
+    {
+        // Fields
+        private string goalName;
+        private decimal estimatedGoalCost;
+        private string optionalGoalDescription;
+
+        // Properties
+        public string GoalName { get; set; }
+        public decimal EstimatedGoalCost { get; set; }
+        public string OptionalGoalDescription { get; set; }
+
+        // Constructor
+        public Goal(string name, decimal cost)
+        {
+            GoalName = name;
+            EstimatedGoalCost = cost;
+        }
+
+        public int CompareTo(Goal otherGoal)
+        {
+            if(this.EstimatedGoalCost == otherGoal.EstimatedGoalCost)
+            {
+                return 0;
+            }
+
+            if(this.EstimatedGoalCost < otherGoal.EstimatedGoalCost)
+            {
+                return -1;
+            }
+
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
