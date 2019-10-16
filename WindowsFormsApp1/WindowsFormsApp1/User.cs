a9558e336eef72c4d5231c562bf13d0e278de537
Omega <theomegakidd@gmail.com>
2019-10-16 15:56:16 -0700

    - Added User.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/User.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    class User
+    {
+        // Fields
+        protected LinkedList<Goal> goals;
+        protected decimal spendingFunds;
+
+        // Properties
+        public LinkedList<Goal> Goals { get; }
+        public decimal SpendingFunds
+        {
+            get
+            {
+                return this.spendingFunds;
+            }
+
+            set
+            {
+                this.spendingFunds = this.spendingFunds + value;
+            }
+        }
+
+        // Constructor
+        public User()
+        {
+            Goals = new LinkedList<Goal>();
+            SpendingFunds = 0.00M;
+        }
+    }
+}
