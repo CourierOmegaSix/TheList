369edfca85b468e0c428e347df7f609abf83cc15
Omega <theomegakidd@gmail.com>
2019-10-22 09:02:39 -0700

    Changed names on UserHome page

--- WindowsFormsApp1/WindowsFormsApp1/UserHome.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/UserHome.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.RemoveGoal_Button = new System.Windows.Forms.Button();
+            this.Cost_ListBox = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Goal_ListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddGoal_Button = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.RemoveFunds_Button = new System.Windows.Forms.Button();
+            this.AddFunds_Button = new System.Windows.Forms.Button();
+            this.SpendingFunds_Label = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.SaveList_Button = new System.Windows.Forms.Button();
+            this.LoadList_Button = new System.Windows.Forms.Button();
+            this.PrintList_Button = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox5.SuspendLayout();
             this.SuspendLayout();
@@ -50,12 +50,12 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Controls.Add(this.RemoveGoal_Button);
+            this.groupBox1.Controls.Add(this.Cost_ListBox);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.Goal_ListBox);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.AddGoal_Button);
             this.groupBox1.Location = new System.Drawing.Point(8, 16);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
@@ -64,34 +64,23 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // listBox1
+            // RemoveGoal_Button
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 24;
-            this.listBox1.Location = new System.Drawing.Point(0, 32);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(200, 388);
-            this.listBox1.TabIndex = 3;
+            this.RemoveGoal_Button.Location = new System.Drawing.Point(168, 440);
+            this.RemoveGoal_Button.Name = "RemoveGoal_Button";
+            this.RemoveGoal_Button.Size = new System.Drawing.Size(144, 32);
+            this.RemoveGoal_Button.TabIndex = 1;
+            this.RemoveGoal_Button.Text = "Remove Goal";
+            this.RemoveGoal_Button.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // Cost_ListBox
             // 
-            this.label1.BackColor = System.Drawing.Color.Silver;
-            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(200, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Goal";
-            // 
-            // listBox2
-            // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 24;
-            this.listBox2.Location = new System.Drawing.Point(200, 32);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(120, 388);
-            this.listBox2.TabIndex = 4;
+            this.Cost_ListBox.FormattingEnabled = true;
+            this.Cost_ListBox.ItemHeight = 16;
+            this.Cost_ListBox.Location = new System.Drawing.Point(200, 32);
+            this.Cost_ListBox.Name = "Cost_ListBox";
+            this.Cost_ListBox.Size = new System.Drawing.Size(120, 388);
+            this.Cost_ListBox.TabIndex = 4;
             // 
             // label2
             // 
@@ -104,30 +93,41 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Cost";
             // 
-            // button2
+            // Goal_ListBox
             // 
-            this.button2.Location = new System.Drawing.Point(168, 440);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Remove Goal";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Goal_ListBox.FormattingEnabled = true;
+            this.Goal_ListBox.ItemHeight = 16;
+            this.Goal_ListBox.Location = new System.Drawing.Point(0, 32);
+            this.Goal_ListBox.Name = "Goal_ListBox";
+            this.Goal_ListBox.Size = new System.Drawing.Size(200, 388);
+            this.Goal_ListBox.TabIndex = 3;
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 440);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add Goal";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Goal";
+            // 
+            // AddGoal_Button
+            // 
+            this.AddGoal_Button.Location = new System.Drawing.Point(8, 440);
+            this.AddGoal_Button.Name = "AddGoal_Button";
+            this.AddGoal_Button.Size = new System.Drawing.Size(144, 32);
+            this.AddGoal_Button.TabIndex = 0;
+            this.AddGoal_Button.Text = "Add Goal";
+            this.AddGoal_Button.UseVisualStyleBackColor = true;
             // 
             // groupBox5
             // 
             this.groupBox5.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox5.Controls.Add(this.button4);
-            this.groupBox5.Controls.Add(this.button3);
-            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.RemoveFunds_Button);
+            this.groupBox5.Controls.Add(this.AddFunds_Button);
+            this.groupBox5.Controls.Add(this.SpendingFunds_Label);
             this.groupBox5.Controls.Add(this.label4);
             this.groupBox5.Location = new System.Drawing.Point(344, 8);
             this.groupBox5.Name = "groupBox5";
@@ -135,33 +135,33 @@
             this.groupBox5.TabIndex = 3;
             this.groupBox5.TabStop = false;
             // 
-            // button4
+            // RemoveFunds_Button
             // 
-            this.button4.Location = new System.Drawing.Point(192, 144);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(152, 32);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Remove Funds";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.RemoveFunds_Button.Location = new System.Drawing.Point(192, 144);
+            this.RemoveFunds_Button.Name = "RemoveFunds_Button";
+            this.RemoveFunds_Button.Size = new System.Drawing.Size(152, 32);
+            this.RemoveFunds_Button.TabIndex = 5;
+            this.RemoveFunds_Button.Text = "Remove Funds";
+            this.RemoveFunds_Button.UseVisualStyleBackColor = true;
+            this.RemoveFunds_Button.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button3
+            // AddFunds_Button
             // 
-            this.button3.Location = new System.Drawing.Point(16, 144);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 32);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Add Funds";
-            this.button3.UseVisualStyleBackColor = true;
+            this.AddFunds_Button.Location = new System.Drawing.Point(16, 144);
+            this.AddFunds_Button.Name = "AddFunds_Button";
+            this.AddFunds_Button.Size = new System.Drawing.Size(152, 32);
+            this.AddFunds_Button.TabIndex = 2;
+            this.AddFunds_Button.Text = "Add Funds";
+            this.AddFunds_Button.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // SpendingFunds_Label
             // 
-            this.label5.BackColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(0, 40);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(360, 88);
-            this.label5.TabIndex = 4;
+            this.SpendingFunds_Label.BackColor = System.Drawing.Color.White;
+            this.SpendingFunds_Label.Location = new System.Drawing.Point(0, 40);
+            this.SpendingFunds_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SpendingFunds_Label.Name = "SpendingFunds_Label";
+            this.SpendingFunds_Label.Size = new System.Drawing.Size(360, 88);
+            this.SpendingFunds_Label.TabIndex = 4;
             // 
             // label4
             // 
@@ -174,43 +174,43 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "Spending Funds";
             // 
-            // button5
+            // SaveList_Button
             // 
-            this.button5.Location = new System.Drawing.Point(344, 216);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(360, 32);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Save List";
-            this.button5.UseVisualStyleBackColor = true;
+            this.SaveList_Button.Location = new System.Drawing.Point(344, 216);
+            this.SaveList_Button.Name = "SaveList_Button";
+            this.SaveList_Button.Size = new System.Drawing.Size(360, 32);
+            this.SaveList_Button.TabIndex = 6;
+            this.SaveList_Button.Text = "Save List";
+            this.SaveList_Button.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // LoadList_Button
             // 
-            this.button6.Location = new System.Drawing.Point(344, 264);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(360, 32);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Load List";
-            this.button6.UseVisualStyleBackColor = true;
+            this.LoadList_Button.Location = new System.Drawing.Point(344, 264);
+            this.LoadList_Button.Name = "LoadList_Button";
+            this.LoadList_Button.Size = new System.Drawing.Size(360, 32);
+            this.LoadList_Button.TabIndex = 7;
+            this.LoadList_Button.Text = "Load List";
+            this.LoadList_Button.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // PrintList_Button
             // 
-            this.button7.Location = new System.Drawing.Point(344, 320);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(360, 32);
-            this.button7.TabIndex = 8;
-            this.button7.Text = "Print List";
-            this.button7.UseVisualStyleBackColor = true;
+            this.PrintList_Button.Location = new System.Drawing.Point(344, 320);
+            this.PrintList_Button.Name = "PrintList_Button";
+            this.PrintList_Button.Size = new System.Drawing.Size(360, 32);
+            this.PrintList_Button.TabIndex = 8;
+            this.PrintList_Button.Text = "Print List";
+            this.PrintList_Button.UseVisualStyleBackColor = true;
             // 
             // UserHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.spiralBlue1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(716, 513);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.PrintList_Button);
+            this.Controls.Add(this.LoadList_Button);
+            this.Controls.Add(this.SaveList_Button);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -232,17 +232,17 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button RemoveGoal_Button;
+        private System.Windows.Forms.Button AddGoal_Button;
         private System.Windows.Forms.GroupBox groupBox5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button RemoveFunds_Button;
+        private System.Windows.Forms.Button AddFunds_Button;
+        private System.Windows.Forms.Label SpendingFunds_Label;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button SaveList_Button;
+        private System.Windows.Forms.Button LoadList_Button;
+        private System.Windows.Forms.Button PrintList_Button;
+        private System.Windows.Forms.ListBox Goal_ListBox;
+        private System.Windows.Forms.ListBox Cost_ListBox;
     }
 }
