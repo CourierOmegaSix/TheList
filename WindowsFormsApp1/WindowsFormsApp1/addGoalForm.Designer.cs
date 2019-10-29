4a31e12830411221f63a767411f6290b245d58b4
Kayla <lil.momma8707@gmail.com>
2019-10-28 19:50:42 -0500

    AddGoalForm: Fixed names for textboxes and buttons. RemoveGoalForm: Added textbox and buttons

--- WindowsFormsApp1/WindowsFormsApp1/addGoalForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/addGoalForm.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.usernameLabel = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.submitButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.fNameBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.goalDescriptionBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.goalCostBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.goalNameBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.addGoalButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,13 +53,13 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.goalDescriptionBox);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.goalCostBox);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.fNameBox);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.submitButton);
+            this.groupBox1.Controls.Add(this.goalNameBox);
+            this.groupBox1.Controls.Add(this.cancelButton);
+            this.groupBox1.Controls.Add(this.addGoalButton);
             this.groupBox1.Controls.Add(this.usernameLabel);
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
@@ -67,51 +67,13 @@
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
-            // submitButton
+            // goalDescriptionBox
             // 
-            this.submitButton.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.submitButton.Location = new System.Drawing.Point(16, 328);
-            this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(120, 43);
-            this.submitButton.TabIndex = 7;
-            this.submitButton.Text = "Add Goal";
-            this.submitButton.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(176, 328);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 43);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Cancel";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // fNameBox
-            // 
-            this.fNameBox.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fNameBox.Location = new System.Drawing.Point(16, 72);
-            this.fNameBox.Name = "fNameBox";
-            this.fNameBox.Size = new System.Drawing.Size(182, 30);
-            this.fNameBox.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 128);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 29);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Cost:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(16, 168);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(182, 30);
-            this.textBox1.TabIndex = 11;
+            this.goalDescriptionBox.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalDescriptionBox.Location = new System.Drawing.Point(16, 264);
+            this.goalDescriptionBox.Name = "goalDescriptionBox";
+            this.goalDescriptionBox.Size = new System.Drawing.Size(182, 30);
+            this.goalDescriptionBox.TabIndex = 13;
             // 
             // label2
             // 
@@ -123,15 +85,55 @@
             this.label2.TabIndex = 12;
             this.label2.Text = "Description:";
             // 
-            // textBox2
+            // goalCostBox
             // 
-            this.textBox2.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(16, 264);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(182, 30);
-            this.textBox2.TabIndex = 13;
+            this.goalCostBox.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalCostBox.Location = new System.Drawing.Point(16, 168);
+            this.goalCostBox.Name = "goalCostBox";
+            this.goalCostBox.Size = new System.Drawing.Size(182, 30);
+            this.goalCostBox.TabIndex = 11;
             // 
-            // addGoalForm
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cost:";
+            // 
+            // goalNameBox
+            // 
+            this.goalNameBox.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalNameBox.Location = new System.Drawing.Point(16, 72);
+            this.goalNameBox.Name = "goalNameBox";
+            this.goalNameBox.Size = new System.Drawing.Size(182, 30);
+            this.goalNameBox.TabIndex = 9;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(176, 328);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 43);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // addGoalButton
+            // 
+            this.addGoalButton.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addGoalButton.Location = new System.Drawing.Point(16, 328);
+            this.addGoalButton.Name = "addGoalButton";
+            this.addGoalButton.Size = new System.Drawing.Size(120, 43);
+            this.addGoalButton.TabIndex = 7;
+            this.addGoalButton.Text = "Add Goal";
+            this.addGoalButton.UseVisualStyleBackColor = true;
+            this.addGoalButton.Click += new System.EventHandler(this.addGoalButton_Click);
+            // 
+            // AddGoalForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -144,7 +146,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "addGoalForm";
+            this.Name = "AddGoalForm";
             this.Text = "Add Goal";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -155,12 +157,12 @@
         #endregion
         private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button submitButton;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button addGoalButton;
+        private System.Windows.Forms.TextBox goalDescriptionBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox goalCostBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox fNameBox;
+        private System.Windows.Forms.TextBox goalNameBox;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/addGoalForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/addGoalForm.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void addGoalButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
